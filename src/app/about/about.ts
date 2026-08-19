b605337541879fd0aec6928f248232e07fba549f
roddaniels <rodbagares.au@gmail.com>
2026-08-19 15:27:16 +1000

    update skills and cert chips

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatChipsModule } from '@angular/material/chips';
+import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-about',
-  imports: [MatListModule, MatChipsModule],
+  imports: [MatListModule, MatChipsModule, ChipModule],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
