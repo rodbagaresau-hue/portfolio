8c29cd150f5db4b931c1fb21b6a0e4a709c9d498
roddaniels <rodbagares.au@gmail.com>
2026-08-19 15:41:30 +1000

    clean code

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { MatChipsModule } from '@angular/material/chips';
 import { ChipModule } from 'primeng/chip';
 
 @Component({
   selector: 'app-about',
-  imports: [MatListModule, MatChipsModule, ChipModule],
+  imports: [MatListModule, ChipModule],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
